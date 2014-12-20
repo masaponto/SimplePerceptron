afb73a9c89b226b16d85fe1359908d97ff9286a2
masaponto <masaponto@gmail.com>
2014-12-21 03:17:02 +0900

    cooler

--- perceptron.hs
+++ perceptron.hs
@@ -1,87 +1,111 @@
 import Graphics.EasyPlot
+import System.Random
 
--- inner product
-innerP :: (Num a) => [a] -> [a] -> a
-innerP a b = sum $ zipWith (*) a b
+type Vector a = [a]
+
+-- vector expression
+-- innner product
+(/*/) :: (Fractional a) => Vector a -> Vector a -> a
+(/*/) xs ys = sum $ zipWith (*) xs ys
+
+-- sum
+(/+/) :: (Fractional a) => Vector a -> Vector a -> Vector a
+(/+/) xs ys = zipWith (+) xs ys
+-- time
+(*/) :: (Fractional a) => a -> Vector a -> Vector a
+(*/) a xs = map (a*) xs
 
 
 -- step function
-step :: Double -> Double -> Double
+step :: (Fractional a ,Ord a) => a -> a -> a
 step n v
     | n >= v = 1
     | otherwise = 0
 
+
 -- discriminal function
-discriFunc :: [Double] -> [Double] -> Double -> Double
-discriFunc w x v = innerP w x - v
+g :: (Fractional a) => Vector a -> Vector a -> a -> a
+g w x v = w /*/ x - v
 
 
 -- update weight
-wUpdate :: Double -> Double -> [Double] -> (Double, [Double]) -> [Double]
-wUpdate r v w xsi = zipWith (+) w $ map (r * (d - y) *) $ snd xsi
-    where d = fst xsi
-          y = step (discriFunc w (snd xsi) v) v
+updateW :: (Fractional a, Ord a) => a -> a -> Vector a -> (a, Vector a) -> Vector a
+updateW r v w xs = w /+/ ( ( r * (d - y) ) */ x)
+    where y = step ( g w x v ) v
+          d = fst xs
+          x = snd xs
 
 
--- is discriminal sucsess
-isMatch :: [Double] -> Double -> (Double, [Double]) -> Bool
-isMatch w v xsi
-    | fst xsi == step (discriFunc w (snd xsi) v) v = True
+-- check results is correct
+isCorrect :: (Fractional a, Ord a) => Vector a -> a -> (a, Vector a) -> Bool
+isCorrect w v xs
+    | d == step (g w x v) v = True
     | otherwise = False
+    where d = fst xs
+          x = snd xs
 
 
 -- training
-train :: [Double] -> Double -> Double ->  [(Double,[Double])] -> [Double]
-train w r v xs
-    | not $ all (isMatch w v) xs = train (foldl (wUpdate r v) w xs) r v xs
-    | otherwise = w
+train :: (Fractional a, Ord a) => Vector a -> a -> a -> [(a, Vector a)] -> Vector a
+train w r v xss
+     | not $ all (isCorrect w v) xss = train (foldl (updateW r v) w xss) r v xss
+     | otherwise = w
 
 
--- change discriminal function for plot
-discriFuncP :: [Double] -> Double -> Double -> [(Double,[Double])] -> Double -> Double
-discriFuncP w r v xs x = a * x + b
+-- discriminal function for plot
+g' :: (Fractional a, Ord a) => Vector a -> a -> a -> [(a , Vector a)] -> a -> a
+g'  w r v xss x = a * x + b
     where a = - (ws !! 1) / (ws !! 2)
           b = - head ws / (ws !! 2)
-          ws = train w r v xs
+          ws = train w r v xss
 
 
--- list of data to vector
-data2vecs :: [(Double, [Double])] -> [(Double, Double)]
-data2vecs datas = zip as bs
-    where as = map ((!! 1).snd) datas
-          bs = map ((!! 2).snd) datas
+-- change vecotr for plot
+data2vecs :: [(a, Vector a)] -> [(a, a)]
+data2vecs xss = zip as bs
+     where as = map ((!! 1).snd) xss
+           bs = map ((!! 2).snd) xss
 
 
--- class separation datalist
-classSepa :: Double -> [(Double, [Double])] -> [(Double, [Double])]
+-- class separation datalist for plot
+classSepa :: (Fractional a, Ord a) => a -> [(a, Vector a)] -> [(a, Vector a)]
 classSepa no = filter (\t -> fst t == no)
 
 
 -- plot discriminal function
-plotDiscriFunc :: [Double] -> Double -> Double -> [(Double,[Double])] -> String -> IO Bool
-plotDiscriFunc wi r v xs ns = plot (PNG ns) [ Function2D [Title $ "discriminal" ++ rate ++ defaultW ++ threshould, Color Red ]
-                                                           [Range (-3) 3] (discriFuncP wi r v xs)
-                                          , Data2D [Title "class1", Color Black] [] class1Points
-                                          , Data2D [Title "class0", Color Blue] [] class0Points]
-    where class1Points = data2vecs $ classSepa 1 xs
-          class0Points = data2vecs $ classSepa 0 xs
-          defaultW = ", w =" ++ show wi
+plotDiscriFunc :: (Fractional a, Ord a, Show a, Enum a) => Vector a -> a -> a -> [( a, Vector a)] -> IO Bool
+plotDiscriFunc wi r v xss = plot (X11) [ Function2D
+                                               [Title $"discriminal" ++ rate ++ threshould, Color Red] [Range (-3) 3] (g' wi r v xss)
+                                           , Data2D [Title "class1", Color Black] [] class1Points
+                                           , Data2D [Title "class0", Color Blue] [] class0Points ]
+    where class1Points = data2vecs $ classSepa 1 xss
+          class0Points = data2vecs $ classSepa 0 xss
           rate = ", r = " ++ show r
           threshould = ", v = " ++ show v
 
-main :: IO Bool
+-- initialize weight vector randomly
+initW :: Int -> IO (Vector Double)
+initW d = newStdGen >>= return . (take d) . (randomRs (-1, 1))
+
+-- add bius
+addBius :: (Num a) => [(a, Vector a)] -> [(a, Vector a)]
+addBius xss = zip (map fst xss) $ map (([1]++).snd) xss
+
+
+main :: IO ()
 main = do
-  putStrLn "please input default weight vector, which type is [Double]"
-  w <- getLine
-  putStrLn "Please input learning rate, which type is [Double]"
-  r <- getLine
-  putStrLn "Please input threshould, which type is Double"
-  v <- getLine
-  putStrLn "Please input learning data, which type is [(Double,[Double])]"
-  line <- getLine
-  putStrLn "Please input save file name, which type is String"
-  fName <- getLine
-  putStrLn "start lerning"
-  _ <- plotDiscriFunc  (read w :: [Double]) (read r :: Double) (read v :: Double) (read line :: [(Double,[Double])]) fName
-  putStrLn "done!!"
-  return True
+  wi <- initW d
+  _ <- plotDiscriFunc wi r v xss
+  return ()
+      where r = 0.5
+            v = 0
+            d = length $ snd $ head xss
+            xss = addBius andData
+            andData = [(1, [1, 1] ),
+                     (0, [1, 0] ),
+                     (0, [0, 1] ),
+                     (0, [0, 0] )]
+            orData = [(1, [1, 1] ),
+                      (1, [1, 0] ),
+                      (1, [0, 1] ),
+                      (0, [0, 0] )]
